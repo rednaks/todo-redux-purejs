60ee5ef4b7a7be7f50611fd6a3d2ff3cb25960f0
SkanderBM <skander.bmahmoud@gmail.com>
2018-09-28 02:04:41 +0200

    visible todo list comp

---
+++ components/VisibleTodoList.js
@@ -0,0 +1,31 @@
+import Element from './Element.js';
+import TodoList from './TodoList.js';
+
+import { VisibilityFilters } from '../state/actions.js';
+
+class VisibleTodoList extends Element {
+  constructor(store) {
+    super();
+    // TODO unsubscribe
+    store.subscribe(() => {
+      const state = store.getState();
+      const todos = this.getVisibleTodoList(state.todos, state.visibilityFilter);
+      const todoList = new TodoList({todos});
+      this._$ = todoList.dom();
+    });
+  }
+  
+  getVisibleTodoList(todos, visibilityFilter) {
+    switch (filter) {
+      case VisibilityFilters.SHOW_ALL:
+        return todos;
+      case VisibilityFilters.SHOW_COMPLETED:
+        return todos.filter(todo => todo.completed);
+      case VisibilityFilters.SHOW_ACTIVE:
+        return todos.filter(todo => !todo.completed);
+      default:
+        throw new Error('Unknown filter: ' + filter);
+  }
+}
+
+export default VisibleTodoList;
