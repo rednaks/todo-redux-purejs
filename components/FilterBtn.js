ee40aa1adabf173a74376993c8c34f993a0562f3
SkanderBM <skander.bmahmoud@gmail.com>
2018-09-28 02:30:34 +0200

    Adding a display name to the filter buttons

--- components/FilterBtn.js
+++ components/FilterBtn.js
@@ -4,10 +4,11 @@ import {
 } from '../states/actions.js'
 
 export default class FilterBtn extends Element {
-  constructor(store, visibilityFilter) {
+  constructor(store, displayName, visibilityFilter) {
     super();
     this._$ = document.createElement('button');
     this._$.disabled = false;
+    this._$.innerHTML = displayName;
     this._$.style.marginLeft = '4px';
     this._visibilityFilter = visibilityFilter;
 
