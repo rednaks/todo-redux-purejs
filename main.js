16b855afb6c302754bc11e0962fd7966ad7a2ad6
SkanderBM <skander.bmahmoud@gmail.com>
2018-09-28 18:42:18 +0200

    cleanup

--- main.js
+++ main.js
@@ -18,17 +18,5 @@ console.log(store.getState());
 
 const unsubscribe = store.subscribe(() => {console.log(store.getState())});
 
-//
-//store.dispatch(addTodo('Learn about actions'));
-//store.dispatch(addTodo('Learn about reducers'));
-//store.dispatch(addTodo('Learn about store'));
-//
-//store.dispatch(toggleTodo(0));
-//store.dispatch(toggleTodo(1));
-//
-//store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
-//console.log(store.getState());
-
-
 
 unsubscribe();
