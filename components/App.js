2534353a5137d7c601b0d4c80083213e1d9e4058
SkanderBM <skander.bmahmoud@gmail.com>
2018-09-28 02:04:59 +0200

    Final app container

---
+++ components/App.js
@@ -0,0 +1,15 @@
+import Element from './Element.js';
+import AddTodo from './AddTodo.js';
+import VisibleTodoList from './VisibleTodoList.js';
+import Footer from './Footer.js';
+
+class App extends Element {
+// WIP
+  constructor(store) {
+    super();
+    this._$ = document.createElement('div');
+    this._$.append(new AddTodo(store).dom());
+    this._$.append(new VisibleTodoList(store).dom());
+    this._$.append(new Footer(store).dom());
+  }
+}
