706ccf1663febfec1d340428bfcd7141e61bc023
SkanderBM <skander.bmahmoud@gmail.com>
2018-09-29 15:47:42 +0200

    Add todo form with bulma

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -9,16 +9,27 @@ export default class AddTodo extends Element {
     this._$ = document.createElement('div');
     
     const _$form =  document.createElement('form');
+    _$form.classList.add('field', 'has-addons');
     this._$.appendChild(_$form);
 
     const _$input = document.createElement('input');
+    _$input.classList.add('input');
+    const _$inputContainer = document.createElement('div');
+    _$inputContainer.classList.add('control');
+    _$inputContainer.appendChild(_$input);
 
     const _$submitBtn = new Button().dom();
     _$submitBtn.type = 'submit';
     _$submitBtn.innerHTML = 'Add';
 
-    _$form.appendChild(_$input);
-    _$form.appendChild(_$submitBtn);
+    const _$submitContainer = document.createElement('div');
+    _$submitContainer.classList.add('control');
+    _$submitContainer.appendChild(_$submitBtn);
+
+
+
+    _$form.appendChild(_$inputContainer);
+    _$form.appendChild(_$submitContainer);
   
     _$form.addEventListener('submit', (e) => {
       e.preventDefault();
